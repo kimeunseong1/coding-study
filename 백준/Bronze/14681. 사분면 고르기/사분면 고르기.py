34a954334370e90ba1bd2b78ddbc7280847f059e
kimeunseong1 <esk0526@gmail.com>
2025-08-17 14:40:26 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,15 @@
+point_x = int(input())
+point_y = int(input())
+
+if point_x > 0 and point_y > 0:
+    print(1)
+elif point_x < 0 and point_y > 0:
+    print(2)
+elif point_x < 0 and point_y < 0:
+    print(3) 
+elif point_x > 0 and point_y < 0:
+    print(4)
+
+
+
+
